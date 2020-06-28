ced79c7bf3796dba08dcbcced3a00eccb5208163
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2020-06-28 20:13:31 -0300

    fix(ios): move out of main thread everything related to instance creation

--- ios/ReactNativeMercadopagoPx.swift
+++ ios/ReactNativeMercadopagoPx.swift
@@ -11,66 +11,66 @@ class ReactNativeMercadopagoPx: NSObject {
 
     @objc(createPayment:resolver:rejecter:)
     func createPayment(options: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-        DispatchQueue.main.async {
-            self.navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController;
+        self.navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController;
                 
-            self.resolver = resolve;
-            self.rejecter = reject;
-                
-            let publicKey = options[JsOptions.PUBLIC_KEY] as! String?;
-            let preferenceId = options[JsOptions.PREFERENCE_ID] as! String?;
+        self.resolver = resolve;
+        self.rejecter = reject;
 
-            if (publicKey == nil) {
-                // TODO: extract error to custom error
-                self.rejecter?(
-                    JsErrorTypes.PUBLIC_KEY_REQUIRED,
-                    "Public key is required for starting MP Checkout",
-                    nil
-                );
-            }
+        let publicKey = options[JsOptions.PUBLIC_KEY] as! String?;
+        let preferenceId = options[JsOptions.PREFERENCE_ID] as! String?;
 
-            if (preferenceId == nil) {
-                // TODO: extract error to custom error
-                self.rejecter?(
-                    JsErrorTypes.PREFERENCE_ID_REQUIRED,
-                    "Preference ID is required for starting MP Checkout",
-                    nil
-                );
-            }
+        if (publicKey == nil) {
+            // TODO: extract error to custom error
+            self.rejecter?(
+                JsErrorTypes.PUBLIC_KEY_REQUIRED,
+                "Public key is required for starting MP Checkout",
+                nil
+            );
+        }
 
-            let builder = MercadoPagoCheckoutBuilder(publicKey: publicKey!, preferenceId: preferenceId!);
+        if (preferenceId == nil) {
+            // TODO: extract error to custom error
+            self.rejecter?(
+                JsErrorTypes.PREFERENCE_ID_REQUIRED,
+                "Preference ID is required for starting MP Checkout",
+                nil
+            );
+        }
 
-            // TODO: add support for language in Android side
-            let language = options[JsOptions.LANGUAGE] as! String?;
+        let builder = MercadoPagoCheckoutBuilder(publicKey: publicKey!, preferenceId: preferenceId!);
 
-            if (language != nil) {
-                builder.setLanguage(language!);
-            }
+        // TODO: add support for language in Android side
+        let language = options[JsOptions.LANGUAGE] as! String?;
 
-            let advancedOptions = options[JsOptions.ADVANCED_OPTIONS] as! NSDictionary?;
+        if (language != nil) {
+            builder.setLanguage(language!);
+        }
 
-            if (advancedOptions != nil) {
-                let config = PXAdvancedConfiguration();
+        let advancedOptions = options[JsOptions.ADVANCED_OPTIONS] as! NSDictionary?;
+
+        if (advancedOptions != nil) {
+            let config = PXAdvancedConfiguration();
                     
-                let productId = advancedOptions![JsOptions.PRODUCT_ID] as! String?;
+            let productId = advancedOptions![JsOptions.PRODUCT_ID] as! String?;
 
-                if productId != nil {
-                    config.setProductId(id: productId!);
-                }
-                    
-                let bankDealsEnabled = advancedOptions?[JsOptions.BANK_DEALS_ENABLED] as! Bool?;
-                let amountRowEnabled = advancedOptions?[JsOptions.AMOUNT_ROW_ENABLED] as! Bool?;
-
-                config.bankDealsEnabled = bankDealsEnabled!;
-                config.amountRowEnabled = amountRowEnabled!;
-                
-                builder.setAdvancedConfiguration(config: config);
+            if productId != nil {
+                config.setProductId(id: productId!);
             }
+                    
+            let bankDealsEnabled = advancedOptions?[JsOptions.BANK_DEALS_ENABLED] as! Bool?;
+            let amountRowEnabled = advancedOptions?[JsOptions.AMOUNT_ROW_ENABLED] as! Bool?;
 
-            // TODO: add support for tracking listener
-            // TODO: add support for customizing fonts
-            // TODO: add support for customizing colors
+            config.bankDealsEnabled = bankDealsEnabled!;
+            config.amountRowEnabled = amountRowEnabled!;
+                
+            builder.setAdvancedConfiguration(config: config);
+        }
+        
+        // TODO: add support for tracking listener
+        // TODO: add support for customizing fonts
+        // TODO: add support for customizing colors
 
+        DispatchQueue.main.async {   
             let checkout = MercadoPagoCheckout(builder: builder);
 
             checkout.start(navigationController: self.navigationController!, lifeCycleProtocol: self);
@@ -81,69 +81,71 @@ class ReactNativeMercadopagoPx: NSObject {
         self.navigationController?.setNavigationBarHidden(true, animated: true);
         self.navigationController?.popToRootViewController(animated: true);
     }
+
+    func cleanUp() -> Void {
+        self.navigationController = nil;
+        self.rejecter = nil;
+        self.resolver = nil;
+    }
 }
 
 extension ReactNativeMercadopagoPx: PXLifeCycleProtocol {
     func finishCheckout() -> ((PXResult?) -> Void)? {
         return ({(_ result: PXResult?) in
-            self.setUpNavigationController();
-            
             if (result == nil) {
                 self.rejecter?(
                     JsErrorTypes.PAYMENT_ERRORED,
                     "Something went wrong when retrieving Payment, please retry",
                     nil
                 );
-                
-                self.rejecter = nil;
-            }
-            
-            var payment: [String : Any?] = [:];
-            
-            if let pxPayment = (result as? PXPayment) {
-                // Default Payment values
-                payment[JsPaymentOptions.ID] = pxPayment.id;
-                payment[JsPaymentOptions.STATUS] = pxPayment.status;
-                payment[JsPaymentOptions.STATUS_DETAIL] = pxPayment.statusDetail;
-                
-                // Additional Payment values
-                payment[JsPaymentOptions.PAYMENT_METHOD_ID] = pxPayment.paymentMethodId;
-                payment[JsPaymentOptions.PAYMENT_TYPE_ID] = pxPayment.paymentTypeId;
-                payment[JsPaymentOptions.ISSUER_ID] = pxPayment.issuerId;
-                payment[JsPaymentOptions.INSTALLMENTS] = pxPayment.installments;
-                payment[JsPaymentOptions.CAPTURED] = pxPayment.captured;
-                payment[JsPaymentOptions.LIVE_MODE] = pxPayment.liveMode;
-                payment[JsPaymentOptions.TRANSACTION_AMOUNT] = String(describing: "\(String(describing: pxPayment.transactionAmount))");
-                payment[JsPaymentOptions.TRANSACTION_DETAILS] = pxPayment.transactionDetails;
             } else {
-                // Default Payment values
-                payment[JsPaymentOptions.ID] = Int(result?.getPaymentId() ?? "");
-                payment[JsPaymentOptions.STATUS] = result?.getStatus();
-                payment[JsPaymentOptions.STATUS_DETAIL] = result?.getStatusDetail();
+                var payment: [String : Any?] = [:];
+                
+                if let pxPayment = (result as? PXPayment) {
+                    // Default Payment values
+                    payment[JsPaymentOptions.ID] = pxPayment.id;
+                    payment[JsPaymentOptions.STATUS] = pxPayment.status;
+                    payment[JsPaymentOptions.STATUS_DETAIL] = pxPayment.statusDetail;
+                    
+                    // Additional Payment values
+                    payment[JsPaymentOptions.PAYMENT_METHOD_ID] = pxPayment.paymentMethodId;
+                    payment[JsPaymentOptions.PAYMENT_TYPE_ID] = pxPayment.paymentTypeId;
+                    payment[JsPaymentOptions.ISSUER_ID] = pxPayment.issuerId;
+                    payment[JsPaymentOptions.INSTALLMENTS] = pxPayment.installments;
+                    payment[JsPaymentOptions.CAPTURED] = pxPayment.captured;
+                    payment[JsPaymentOptions.LIVE_MODE] = pxPayment.liveMode;
+                    payment[JsPaymentOptions.TRANSACTION_AMOUNT] = String(describing: "\(String(describing: pxPayment.transactionAmount))");
+                    payment[JsPaymentOptions.TRANSACTION_DETAILS] = pxPayment.transactionDetails;
+                } else {
+                    // Default Payment values
+                    payment[JsPaymentOptions.ID] = Int(result?.getPaymentId() ?? "");
+                    payment[JsPaymentOptions.STATUS] = result?.getStatus();
+                    payment[JsPaymentOptions.STATUS_DETAIL] = result?.getStatusDetail();
+                }
+                
+                self.resolver?(payment);
             }
-            
-            self.resolver?(payment);
-            self.resolver = nil;
+    
+            self.setUpNavigationController();
+            self.cleanUp();
         });
     }
 
     func cancelCheckout() -> (() -> Void)? {
         return {
-            self.setUpNavigationController();
             self.rejecter?(
                 JsErrorTypes.PAYMENT_CANCELLED,
                 "Payment was cancelled by the user",
                 nil
             );
             
-            self.rejecter = nil;
+            self.setUpNavigationController();
+            self.cleanUp();
         };
     }
 }
 
-// Options for JS Module
 enum JsOptions {
-    
     // Required Options
     static let PUBLIC_KEY = "publicKey";
     static let PREFERENCE_ID = "preferenceId";
@@ -156,9 +158,7 @@ enum JsOptions {
     static let AMOUNT_ROW_ENABLED = "amountRowEnabled";
 }
 
-// JS Exposed Error Types
 enum JsErrorTypes {
-    
     // Payment Error Types
     static let PAYMENT_ERRORED = "mp:payment_errored";
     static let PAYMENT_CANCELLED = "mp:payment_cancelled";
@@ -168,9 +168,7 @@ enum JsErrorTypes {
     static let PREFERENCE_ID_REQUIRED = "mp:preference_id_required";
 }
 
-// JS Payment Options
 enum JsPaymentOptions {
-    
     // Default
     static let ID = "id";
     static let STATUS = "status";
